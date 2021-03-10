355c20a8ee6147673f81bb34396c555cf8b1de35
jlstoupy98 <jean-luc.stoupy@epfl.ch>
2021-03-10 20:50:15 +0100

    Theme and logo test

--- app/src/main/java/com/sdp/movemeet/HomeScreenActivity.java
+++ app/src/main/java/com/sdp/movemeet/HomeScreenActivity.java
@@ -3,6 +3,7 @@ package com.sdp.movemeet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 
