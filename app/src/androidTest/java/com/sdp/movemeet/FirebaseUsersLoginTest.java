1bfdbbec692e817097978e32829cfc6bbe45b04b
Anthony Guinchard <antho.guinchard@gmail.com>
2021-03-11 21:07:54 +0100

    Add test for LoginActivity

---
+++ app/src/androidTest/java/com/sdp/movemeet/FirebaseUsersLoginTest.java
@@ -0,0 +1,56 @@
+package com.sdp.movemeet;
+
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.containsString;
+
+
+@RunWith(AndroidJUnit4.class)
+public class FirebaseUsersLoginTest {
+
+    public static final String KEYBOARD_INPUT_EMAIL = "antho2@gmail.com";
+    public static final String KEYBOARD_INPUT_PASSWORD = "234567";
+
+    @Rule
+    public ActivityScenarioRule<FirebaseUsersLogin> mActivityTestRule = new ActivityScenarioRule<>(FirebaseUsersLogin.class);
+
+    @Test
+    public void firebaseUsersLoginEndToEnd() {
+
+        // Filling the email
+        onView(ViewMatchers.withId(R.id.edit_text_email)).perform(typeText(KEYBOARD_INPUT_EMAIL), closeSoftKeyboard());
+
+        // Filling the password
+        onView(ViewMatchers.withId(R.id.edit_text_password)).perform(typeText(KEYBOARD_INPUT_PASSWORD), closeSoftKeyboard());
+
+        // Clicking on the LOGIN button
+        onView(withId(R.id.text_view_details_glance)).perform(click());
+
+        // Sleeping a bit while sending request to Firebase
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            assert (false);
+        }
+
+        // Checking the correct update of the textViews
+        onView(ViewMatchers.withId(R.id.text_view_profile_name)).check(matches(withText(containsString("Anthony Test2"))));
+        onView(ViewMatchers.withId(R.id.text_view_profile_email)).check(matches(withText(containsString("antho2@gmail.com"))));
+        onView(ViewMatchers.withId(R.id.text_view_profile_phone)).check(matches(withText(containsString("+41798841817"))));
+
+    }
+
+}
--- app/src/main/java/com/sdp/movemeet/FirebaseUsersLogin.java
+++ app/src/main/java/com/sdp/movemeet/FirebaseUsersLogin.java
@@ -1,11 +1,9 @@
 package com.sdp.movemeet;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,18 +11,30 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class FirebaseUsersLogin extends AppCompatActivity {
 
     EditText mEmail, mPassword;
-    Button mLoginBtn;
-    TextView mCreateBtn;
+    TextView mCreateBtn, mDetailsGlanceBtn;
     ProgressBar progressBar;
     FirebaseAuth fAuth;
+
+    TextView fullName, emailTextView, phone;
+    FirebaseFirestore fStore;
+    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,14 +45,23 @@ public class FirebaseUsersLogin extends AppCompatActivity {
         mPassword = findViewById(R.id.edit_text_password);
         progressBar = findViewById(R.id.progressBar);
         fAuth = FirebaseAuth.getInstance();
-        mLoginBtn = findViewById(R.id.button_login);
         mCreateBtn = findViewById(R.id.text_view_create_account);
+        mDetailsGlanceBtn = findViewById(R.id.text_view_details_glance);
 
-        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+
+        // Defining the OnClickListener for the "text_view_create_account button"
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), FirebaseUsersRegister.class)); // redirecting the user to the "Register" activity
+            }
+        });
 
-                // When the user clicks on the "LOGIN" button, we first validate his data
+        // Defining the OnClickListener for the "text_view_details_glance button"
+        mDetailsGlanceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // When the user clicks on the "text_view_details_glance button" button, we first validate his data
                 String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
 
@@ -68,8 +87,33 @@ public class FirebaseUsersLogin extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Toast.makeText(FirebaseUsersLogin.this, "Logged in successfully.", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), FirebaseUsersMainActivity.class)); // we redirect the user to the "MainActivity"
+                            Toast toast = Toast.makeText(FirebaseUsersLogin.this, "Account verified.", Toast.LENGTH_SHORT);
+                            toast.setGravity(Gravity.CENTER_VERTICAL | Gravity.START, 90, 0);
+                            toast.show();
+
+                            // Displaying user data
+                            //----
+                            phone = findViewById(R.id.text_view_profile_phone);
+                            fullName = findViewById(R.id.text_view_profile_name);
+                            emailTextView = findViewById(R.id.text_view_profile_email);
+
+                            fAuth = FirebaseAuth.getInstance();
+                            fStore = FirebaseFirestore.getInstance();
+
+                            userId = fAuth.getCurrentUser().getUid();
+
+                            // Retrieving the data from the Firestore database
+                            DocumentReference documentReference = fStore.collection("users").document(userId);
+                            documentReference.addSnapshotListener(FirebaseUsersLogin.this, new EventListener<DocumentSnapshot>() {
+                                @Override
+                                public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                                    phone.setText(documentSnapshot.getString("phone"));
+                                    fullName.setText(documentSnapshot.getString("fullName"));
+                                    emailTextView.setText(documentSnapshot.getString("email"));
+                                }
+
+                            });
+                            //----
 
                         } else {
                             Toast.makeText(FirebaseUsersLogin.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
@@ -79,15 +123,6 @@ public class FirebaseUsersLogin extends AppCompatActivity {
 
                 });
 
-            }
-
-        });
-
-        // Defining the OnClickListener for the "text view button"
-        mCreateBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), FirebaseUsersRegister.class)); // redirecting the user to the "Register" activity
             }
         });
 
