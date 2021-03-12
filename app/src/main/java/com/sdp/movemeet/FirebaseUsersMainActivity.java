802538e1026ef7b397365f426bc4d6ea0b7404da
Anthony Guinchard <antho.guinchard@gmail.com>
2021-03-12 18:05:33 +0100

    Add Firebase user data implementation

--- app/src/main/java/com/sdp/movemeet/FirebaseUsersMainActivity.java
+++ app/src/main/java/com/sdp/movemeet/FirebaseUsersMainActivity.java
@@ -4,21 +4,41 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.sdp.movemeet.Backend.BackendActivityManagerDemo;
+import com.sdp.movemeet.Backend.FirebaseInteraction;
 
 
 public class FirebaseUsersMainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.sdp.movemeet.MESSAGE";
 
+    TextView fullName, email, phone;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        phone = findViewById(R.id.text_view_profile_phone);
+        fullName = findViewById(R.id.text_view_profile_name);
+        email = findViewById(R.id.text_view_profile_email);
+
+        handleRegisterUser();
+
     }
 
     /* Called when the user taps the Go button */
@@ -42,4 +62,16 @@ public class FirebaseUsersMainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, BackendActivityManagerDemo.class);
         startActivity(intent);
     }
+
+    public void handleRegisterUser() {
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        if (fAuth.getCurrentUser() != null) {
+            userId = fAuth.getCurrentUser().getUid();
+            TextView[] textViewArray = {phone, fullName, email};
+            FirebaseInteraction.retrieveDataFromFirebase(fStore, userId, textViewArray, FirebaseUsersMainActivity.this);
+        }
+
+    }
+
 }
