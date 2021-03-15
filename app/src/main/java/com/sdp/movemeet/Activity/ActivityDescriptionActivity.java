06a9ee14d4989cca7b408e7e7398be18db89fd37
jlstoupy98 <jean-luc.stoupy@epfl.ch>
2021-03-15 21:43:46 +0100

    activity description

---
+++ app/src/main/java/com/sdp/movemeet/Activity/ActivityDescriptionActivity.java
@@ -0,0 +1,55 @@
+package com.sdp.movemeet.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.sdp.movemeet.R;
+import com.sdp.movemeet.Sport;
+
+import java.text.BreakIterator;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class ActivityDescriptionActivity extends AppCompatActivity {
+
+    public Activity act = new Activity("activityId",
+                     "organizerId",
+                     "Snowboard",
+                    5,
+                    new ArrayList<>(),
+                    004,
+                    005,
+                    "BlaBlaBla Description de l'activité",
+                    new Date(),
+                    90,
+                    Sport.Running,
+                    "address");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_description);
+        // activityTitle from the activity
+        TextView activityTitle = (TextView) findViewById(R.id.activityTitle);
+        activityTitle.setText(act.getTitle());
+        // number of participants from the activity
+        TextView numberParticipants = (TextView) findViewById(R.id.numberParticipant);
+        numberParticipants.setText("Participants : " + act.getParticipantId().size() + "/" + act.getNumberParticipant());
+        // description from the activity
+        TextView description = (TextView) findViewById(R.id.description);
+        description.setText(act.getDescription());
+        // date from the activity
+        TextView date = (TextView) findViewById(R.id.date);
+        String pattern = "MM/dd/yyyy HH:mm:ss";
+        DateFormat df = new SimpleDateFormat(pattern);
+        String todayAsString = df.format(act.getDate());
+        date.setText(todayAsString);
+        // address from the activity
+        TextView address = (TextView) findViewById(R.id.address);
+        address.setText(act.getAddress());
+    }
+}
