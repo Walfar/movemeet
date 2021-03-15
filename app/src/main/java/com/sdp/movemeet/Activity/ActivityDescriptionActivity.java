df16be44631a2077f9fdd754d7bc1f2f104deff0
jlstoupy98 <jean-luc.stoupy@epfl.ch>
2021-03-15 21:51:59 +0100

    activity description

--- app/src/main/java/com/sdp/movemeet/Activity/ActivityDescriptionActivity.java
+++ app/src/main/java/com/sdp/movemeet/Activity/ActivityDescriptionActivity.java
@@ -33,23 +33,47 @@ public class ActivityDescriptionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_description);
+
+        createTitleView();
+        createParticipantNumberView();
+        createDescriptionView();
+        createDateView();
+        createAddressView();
+    }
+
+    private void createTitleView(){
         // activityTitle from the activity
         TextView activityTitle = (TextView) findViewById(R.id.activityTitle);
         activityTitle.setText(act.getTitle());
+    }
+
+    private void createParticipantNumberView(){
         // number of participants from the activity
         TextView numberParticipants = (TextView) findViewById(R.id.numberParticipant);
         numberParticipants.setText("Participants : " + act.getParticipantId().size() + "/" + act.getNumberParticipant());
+    }
+
+    private void createDescriptionView(){
         // description from the activity
         TextView description = (TextView) findViewById(R.id.description);
         description.setText(act.getDescription());
+    }
+
+    private void createDateView(){
         // date from the activity
         TextView date = (TextView) findViewById(R.id.date);
         String pattern = "MM/dd/yyyy HH:mm:ss";
         DateFormat df = new SimpleDateFormat(pattern);
         String todayAsString = df.format(act.getDate());
         date.setText(todayAsString);
+    }
+
+    private void createAddressView(){
         // address from the activity
         TextView address = (TextView) findViewById(R.id.address);
         address.setText(act.getAddress());
     }
+
+
+
 }
