0a036438c8ec880fc820d770ac6625026cea4dd4
RoxaneBurri <roxane.burri@etu.unige.ch>
2021-03-17 11:08:13 +0100

    activity screen description

---
+++ app/src/main/java/com/sdp/movemeet/map/ActivityDescriptionActivity.java
@@ -0,0 +1,18 @@
+package com.sdp.movemeet.map;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import com.sdp.movemeet.map.MapsActivity;
+
+
+public class ActivityDescriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_description);
+    }
+}
