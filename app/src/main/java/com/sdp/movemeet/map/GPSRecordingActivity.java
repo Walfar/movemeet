b5ad64f99197e604f8f3165f85541b6f2a5ad958
kepler <k.warrington.arroyo@gmail.com>
2021-03-22 16:18:27 +0100

    Created branch for GPS recording

---
+++ app/src/main/java/com/sdp/movemeet/map/GPSRecordingActivity.java
@@ -0,0 +1,16 @@
+package com.sdp.movemeet.map;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sdp.movemeet.R;
+
+public class GPSRecordingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_g_p_s_recording);
+    }
+}
