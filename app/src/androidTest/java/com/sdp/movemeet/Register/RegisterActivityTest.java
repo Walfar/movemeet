fef60b0c30771de828419554c54d5dfb984ada54
jlstoupy98 <jean-luc.stoupy@epfl.ch>
2021-03-23 23:05:26 +0100

    improve test

--- app/src/androidTest/java/com/sdp/movemeet/Register/RegisterActivityTest.java
+++ app/src/androidTest/java/com/sdp/movemeet/Register/RegisterActivityTest.java
@@ -1,22 +1,35 @@
 package com.sdp.movemeet.Register;
 
+import androidx.annotation.NonNull;
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.sdp.movemeet.HomeScreenActivity;
 import com.sdp.movemeet.R;
 import com.sdp.movemeet.RegisterActivity;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.concurrent.CountDownLatch;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static java.sql.DriverManager.println;
 
 @RunWith(AndroidJUnit4.class)
 public class RegisterActivityTest {
@@ -34,6 +47,11 @@ public class RegisterActivityTest {
     @Test
     public void Empty_Register(){
         onView(withId(R.id.button_register)).perform(click());
+    }
+
+    @Test
+    public void goToLogin(){
+        onView(withId(R.id.text_view_login_here)).perform(click());
     }
 
     @Test
@@ -69,5 +87,4 @@ public class RegisterActivityTest {
                 .perform(typeText(mPassword), closeSoftKeyboard());
         onView(withId(R.id.button_register)).perform(click());
     }
-
 }
