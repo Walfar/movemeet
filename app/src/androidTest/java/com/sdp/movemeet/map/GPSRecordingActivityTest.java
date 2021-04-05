c775e0b23e51adf7f903ae95b799a95e77b950ca
kepler <k.warrington.arroyo@gmail.com>
2021-04-05 07:53:46 +0200

    Added asserFalse() to empty path test

--- app/src/androidTest/java/com/sdp/movemeet/map/GPSRecordingActivityTest.java
+++ app/src/androidTest/java/com/sdp/movemeet/map/GPSRecordingActivityTest.java
@@ -33,6 +33,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
@@ -75,7 +76,7 @@ public class GPSRecordingActivityTest {
         onView(withId(R.id.recordButton)).check(matches(withText("Start")));
 
         scenario.onActivity(activity -> {
-            assertNotEquals("Path was empty", true, ((GPSRecordingActivity) activity).path.isEmpty());
+            assertFalse("Path was empty", ((GPSRecordingActivity) activity).path.isEmpty());
         });
     }
 
