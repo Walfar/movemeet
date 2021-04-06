dad586ec4112368ae26d0745971806e051dcebba
Walfar <victor.carles@epfl.ch>
2021-04-06 21:21:42 +0200

    merge with main

--- app/src/main/java/com/sdp/movemeet/UploadActivityActivity.java
+++ app/src/main/java/com/sdp/movemeet/UploadActivityActivity.java
@@ -109,24 +109,18 @@ public class UploadActivityActivity extends AppCompatActivity {
         }
 
     }
-
     private void setupSportSpinner(Context context) {
         spinner = (Spinner) findViewById(R.id.spinnerSportType);
-
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter adapter = new ArrayAdapter(context,
                 android.R.layout.simple_spinner_dropdown_item, Sport.values());
-
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
     }
 
-
     // Helper methods for date picker
-
     private void setupDateInput(Context context) {
         dateText = findViewById(R.id.editTextDate);
         calendar = Calendar.getInstance();
@@ -336,6 +330,5 @@ public class UploadActivityActivity extends AppCompatActivity {
                 });
         startActivity(new Intent(getApplicationContext(), MainMapFragment.class));
     }
-
 
 }
--- app/src/main/java/com/sdp/movemeet/map/GPSRecordingActivity.java
+++ app/src/main/java/com/sdp/movemeet/map/GPSRecordingActivity.java
@@ -206,6 +206,7 @@ public class GPSRecordingActivity extends FragmentActivity implements OnMapReady
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
             case REQUEST_CODE:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
