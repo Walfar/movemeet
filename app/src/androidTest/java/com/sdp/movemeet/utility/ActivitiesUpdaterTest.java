b871b11e4707109720d88ab88d02dfaa7fdd0a80
Walfar <victor.carles@epfl.ch>
2021-04-15 18:44:55 +0200

    added sports + createdAt attribute to activity (for ordering)

--- app/src/androidTest/java/com/sdp/movemeet/utility/ActivitiesUpdaterTest.java
+++ app/src/androidTest/java/com/sdp/movemeet/utility/ActivitiesUpdaterTest.java
@@ -60,7 +60,8 @@ public class ActivitiesUpdaterTest {
                 new Date(),
                 10,
                 Sport.Running,
-                "address");
+                "address",
+                new Date());
 
         bam.uploadActivity(act,
                 new OnSuccessListener<Void>() {
