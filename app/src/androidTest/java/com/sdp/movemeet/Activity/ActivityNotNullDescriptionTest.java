b6ea12dd7182149256a37aee39b1ac88759ac92a
jlstoupy98 <jean-luc.stoupy@epfl.ch>
2021-04-20 19:00:05 +0200

    block access if user is not logged

--- app/src/androidTest/java/com/sdp/movemeet/Activity/ActivityNotNullDescriptionTest.java
+++ app/src/androidTest/java/com/sdp/movemeet/Activity/ActivityNotNullDescriptionTest.java
@@ -4,21 +4,37 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.sdp.movemeet.Backend.BackendActivityManager;
 import com.sdp.movemeet.R;
 import com.sdp.movemeet.Sport;
+import com.sdp.movemeet.UploadActivityActivity;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 ;import java.util.ArrayList;
 import java.util.Date;
+import java.util.concurrent.CountDownLatch;
 
 import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 
@@ -37,6 +53,8 @@ public class ActivityNotNullDescriptionTest {
     private final static double DUMMY_DURATION = 10.4;
     private final static Sport DUMMY_SPORT = Sport.Running;
     private final static String DUMMY_ADDRESS = "address";
+    public FirebaseAuth mAuth;
+    public String uid;
 
     private Activity activity = new Activity(
             DUMMY_ACTIVITY_ID,
@@ -54,13 +72,91 @@ public class ActivityNotNullDescriptionTest {
             DUMMY_DATE
     );
 
-    @Rule
-    public ActivityScenarioRule<ActivityDescriptionActivity> testRule = new ActivityScenarioRule<>(new Intent(getApplicationContext(), ActivityDescriptionActivity.class).putExtra("activity", activity));
+    //@Rule
+    //public ActivityScenarioRule<ActivityDescriptionActivity> testRule = new ActivityScenarioRule<>(new Intent(getApplicationContext(), ActivityDescriptionActivity.class).putExtra("activity", activity));
+
+    @Before
+    public void signIn() {
+        CountDownLatch latch = new CountDownLatch(1);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuth.signInWithEmailAndPassword("movemeet@gmail.com", "password").addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    sleep(500);
+                    latch.countDown();
+                } else {
+                    assert (false);
+                }
+            }
+        });
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            assert (false);
+        }
+    }
 
     @Test
     public void create() {
+        ActivityScenarioRule<ActivityDescriptionActivity> testRule = new ActivityScenarioRule<>(new Intent(getApplicationContext(), ActivityDescriptionActivity.class).putExtra("activity", activity));
+        //ActivityScenario scenario = testRule.getScenario();
         Intents.init();
         Intents.release();
+    }
+
+    @After
+    public void deleteAndSignOut() {
+        CountDownLatch latch = new CountDownLatch(1);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        BackendActivityManager bam = new BackendActivityManager(db, BackendActivityManager.ACTIVITIES_COLLECTION);
+
+        bam.getActivitiesCollectionReference()
+                .whereEqualTo("organizerId", mAuth.getCurrentUser().getUid())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        for (DocumentSnapshot docSnap: queryDocumentSnapshots.getDocuments()) {
+                            docSnap.getReference().delete()
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            // Silently fail if document couldn't be deleted ->
+                                            // prevent simultaneous deletions causing problems
+                                            latch.countDown();
+                                        }
+                                    });
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                assert(false);
+            }
+        });
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            assert(false);
+        }
+
+        mAuth.signOut();
+    }
+
+    public boolean sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+            return true;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
 }
