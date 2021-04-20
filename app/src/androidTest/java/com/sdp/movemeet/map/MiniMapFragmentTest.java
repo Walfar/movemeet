da24533ad1b2947014cc11156aa698ec5cdff524
Walfar <victor.carles@epfl.ch>
2021-04-20 03:25:24 +0200

    started reworking mini map tests

--- app/src/androidTest/java/com/sdp/movemeet/map/MiniMapFragmentTest.java
+++ app/src/androidTest/java/com/sdp/movemeet/map/MiniMapFragmentTest.java
@@ -1,6 +1,7 @@
 package com.sdp.movemeet.map;
 
 import android.Manifest;
+import android.location.Location;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -17,6 +18,7 @@ import androidx.test.uiautomator.UiObjectNotFoundException;
 import androidx.test.uiautomator.UiSelector;
 
 import com.android21buttons.fragmenttestrule.FragmentTestRule;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -34,6 +36,8 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
@@ -47,34 +51,47 @@ public class MiniMapFragmentTest {
     public GrantPermissionRule mRuntimePermissionRule = GrantPermissionRule.grant(Manifest.permission.ACCESS_FINE_LOCATION);
 
     @Rule
-    public ActivityScenarioRule<UploadActivityActivity> testRule = new ActivityScenarioRule<>(UploadActivityActivity.class);
-
-    @Rule
-    public ActivityTestRule rule = new ActivityTestRule<>(UploadActivityActivity.class);
-
-    @Rule
-    public FragmentTestRule<?, MiniMapFragment> fragmentTestRule =
-            FragmentTestRule.create(MiniMapFragment.class);
+    public ActivityScenarioRule<UploadActivityActivity> rule = new ActivityScenarioRule<>(UploadActivityActivity.class);
 
     @Before
     public void setUp() throws InterruptedException {
-        rule.getActivity().getFragmentManager().beginTransaction();
-        Thread.sleep(2000);
         fAuth = FirebaseAuth.getInstance();
         fAuth.signInWithEmailAndPassword("test@test.com", "password").addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
                 user = fAuth.getCurrentUser();
+                uiDevice = UiDevice.getInstance(getInstrumentation());
             }
         });
-        Thread.sleep(2000);
+        sleep(2000);
     }
-
 
     @Test
     public void miniMapFragment_isDisplayed() throws InterruptedException {
-        onView(withId(R.id.fragment_map)).check(matches((isDisplayed())));
+        onView(withId(R.id.fragment_container_view)).check(matches((isDisplayed())));
+    }
+
+    /*
+    @Test
+    public void miniMapFragment_onClickSetsLocation() {
+        //What could be the right coords of the mini map ?
+        uiDevice.click(uiDevice.getDisplaySizeDp().x/2, uiDevice.getDisplaySizeDp().y);
+        sleep(2000);
+        rule.getScenario().onActivity(activity -> {
+            LatLng address = activity.getAddressLocation();
+            assertNotNull(address);
+            assertEquals(0, address.latitude, 0);
+            assertEquals(0, address.longitude, 0);
+        });
+    } */
+
+    public boolean sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+            return true;
+        } catch (InterruptedException e) {
+            return false;
+        }
     }
 
 }
