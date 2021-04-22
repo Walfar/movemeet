fac6166a20c466aea054f7df775460b284eb1997
RoxaneBurri <roxane.burri@etu.unige.ch>
2021-04-22 18:57:37 +0200

    fix error

--- app/src/main/java/com/sdp/movemeet/HomeUnregister.java
+++ app/src/main/java/com/sdp/movemeet/HomeUnregister.java
@@ -13,28 +13,21 @@ import android.widget.TextView;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.sdp.movemeet.Backend.FirebaseInteraction;
 import com.sdp.movemeet.utility.ActivitiesUpdater;
 
 public class HomeUnregister extends AppCompatActivity {
     DrawerLayout drawerLayout;
     Toolbar toolbar;
     TextView textView;
-    FirebaseAuth fAuth;
-
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_unregister);
 
-
         createDrawer();
-
-        /*if(fAuth.getCurrentUser() == null){
-            startActivity(new Intent(getApplicationContext(), LoginActivity.class)); // sending the user to the "Login" activity
-            finish();
-        }*/
-
     }
 
     public void createDrawer(){
