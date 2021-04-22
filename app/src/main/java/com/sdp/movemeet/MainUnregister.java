569bfcf046b1a9168e950f2fdc8cf1acace7eab1
RoxaneBurri <roxane.burri@etu.unige.ch>
2021-04-22 21:38:38 +0200

    change class name

--- app/src/main/java/com/sdp/movemeet/MainUnregister.java
+++ app/src/main/java/com/sdp/movemeet/MainUnregister.java
@@ -1,22 +1,13 @@
 package com.sdp.movemeet;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
-import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.sdp.movemeet.Backend.FirebaseInteraction;
-import com.sdp.movemeet.utility.ActivitiesUpdater;
-
-public class HomeUnregister extends AppCompatActivity {
+public class MainUnregister extends AppCompatActivity {
     DrawerLayout drawerLayout;
     Toolbar toolbar;
     TextView textView;
@@ -25,7 +16,7 @@ public class HomeUnregister extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_unregister);
+        setContentView(R.layout.activity_main_unregister);
 
         createDrawer();
     }
