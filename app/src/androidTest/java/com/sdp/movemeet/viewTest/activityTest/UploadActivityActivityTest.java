9d38e7b975c08bf7f57f43ae2b2afec38e5af6a6
kepler <k.warrington.arroyo@gmail.com>
2021-05-06 20:59:40 +0200

    Started rewriting ActivityUpload tests

--- app/src/androidTest/java/com/sdp/movemeet/viewTest/activityTest/UploadActivityActivityTest.java
+++ app/src/androidTest/java/com/sdp/movemeet/viewTest/activityTest/UploadActivityActivityTest.java
@@ -4,60 +4,74 @@ import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 
-import androidx.annotation.NonNull;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.UiController;
 import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.contrib.PickerActions;
+import androidx.test.espresso.intent.Intents;
 import androidx.test.filters.LargeTest;
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.sdp.movemeet.backend.BackendActivityManager;
 import com.sdp.movemeet.R;
 import com.sdp.movemeet.backend.firebase.firestore.FirestoreActivityManager;
-import com.sdp.movemeet.backend.serialization.ActivitySerializer;
+import com.sdp.movemeet.backend.firebase.firestore.FirestoreUserManager;
+import com.sdp.movemeet.backend.providers.AuthenticationInstanceProvider;
+import com.sdp.movemeet.backend.providers.BackendInstanceProvider;
 import com.sdp.movemeet.backend.serialization.BackendSerializer;
+import com.sdp.movemeet.backend.serialization.UserSerializer;
 import com.sdp.movemeet.models.Activity;
+import com.sdp.movemeet.models.User;
 import com.sdp.movemeet.view.activity.UploadActivityActivity;
+import com.sdp.movemeet.view.home.HomeScreenActivity;
+import com.sdp.movemeet.view.main.MainActivity;
 
 import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.util.concurrent.CountDownLatch;
+import org.mockito.ArgumentCaptor;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.contrib.DrawerMatchers.isClosed;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.hamcrest.Matchers.nullValue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(AndroidJUnit4ClassRunner.class)
 public class UploadActivityActivityTest {
 
-    public FirebaseAuth mAuth;
-    public FirebaseFirestore db;
-    public BackendSerializer<Activity> activitySerializer;
-    public FirestoreActivityManager backendActivityManager;
+    private Task<DocumentSnapshot> getUserTask;
+
+    private BackendSerializer<Activity> activitySerializer;
+    private FirestoreActivityManager backendActivityManager;
+
 
     public static ViewAction forceDoubleClick() {
         return new ViewAction() {
@@ -80,40 +94,77 @@ public class UploadActivityActivityTest {
 
     @Before
     public void setup() {
-        CountDownLatch latch = new CountDownLatch(1);
+        BackendInstanceProvider.firestore = mock(FirebaseFirestore.class);
+        AuthenticationInstanceProvider.fAuth = mock(FirebaseAuth.class);
 
-        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mock(FirebaseUser.class);
+        String uid = "uid";
 
-        mAuth.signInWithEmailAndPassword("movemeet@gmail.com", "password").addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    sleep(500);
-                    latch.countDown();
-                } else {
-                    assert (false);
-                }
-            }
-        });
+        when(AuthenticationInstanceProvider.fAuth.getCurrentUser()).thenReturn(user);
+        when(user.getUid()).thenReturn(uid);
 
-        try {
-            latch.await();
-        } catch (InterruptedException e) {
-            assert (false);
-        }
-
-        activitySerializer = new ActivitySerializer();
-        backendActivityManager = new FirestoreActivityManager(
-                FirebaseFirestore.getInstance(),
-                FirestoreActivityManager.ACTIVITIES_COLLECTION,
-                activitySerializer
-        );
+        Intents.init();
     }
 
-    // This test has to take extra time or the Views won't update fast enough and it'll fail on CI
     @Test
-    @LargeTest
-    public void endToEnd() {
+    public void confirmActivityUploadClosesActivityOnSuccess() {
+        ActivityScenario scenario = ActivityScenario.launch(UploadActivityActivity.class);
+
+        Activity mockActivity = mock(Activity.class);
+
+        scenario.onActivity(activity -> {
+            UploadActivityActivity spyActivity = spy((UploadActivityActivity) activity);
+
+            
+        });
+    }
+
+
+
+    /*@Test
+    public void confirmActivityUploadClosesActivityOnSuccess() {
+
+        ArgumentCaptor<OnSuccessListener> successCaptor = ArgumentCaptor.forClass(OnSuccessListener.class);
+
+        //ArgumentCaptor<OnCompleteListener> completeCaptor = ArgumentCaptor.forClass(OnCompleteListener.class);
+
+        UploadActivityActivity.enableNav = false;
+
+        ActivityScenario scenario = ActivityScenario.launch(UploadActivityActivity.class);
+
+        //verify(getUserTask).addOnCompleteListener(completeCaptor.capture());
+        //completeCaptor.getValue().onComplete(getUserTask);
+
+        Activity act = mock(Activity.class);
+        Task addTask = mock(Task.class);
+
+        scenario.onActivity(activity -> {
+            //when(((UploadActivityActivity) activity).validateActivity()).thenReturn(act);
+            //((UploadActivityActivity) activity)::validateActivity = this::getFakeActivity;
+            doReturn(act).when(((UploadActivityActivity) activity))
+            when(((UploadActivityActivity) activity).activityBackendManager.add(any(), any()))
+                    .thenReturn(addTask);
+
+            ((UploadActivityActivity) activity).confirmActivityUpload(null);
+
+            verify(addTask).addOnSuccessListener(successCaptor.capture());
+
+            successCaptor.getValue().onSuccess(null);
+
+            intended(hasComponent(MainActivity.class.getName()));
+        });
+    }*/
+
+    @After
+    public void tearDown() {
+        Intents.release();
+    }
+
+
+    // This test has to take extra time or the Views won't update fast enough and it'll fail on CI
+    //@Test
+    //@LargeTest
+    /*public void endToEnd() {
         ActivityScenario scenario = ActivityScenario.launch(UploadActivityActivity.class);
 
         sleep(1000);
@@ -159,7 +210,7 @@ public class UploadActivityActivityTest {
 
         scenario.onActivity(activity -> {
             assert (((UploadActivityActivity) activity).validLocation == true);
-            assert (((UploadActivityActivity) activity).validStartTime == false);
+            //assert (((UploadActivityActivity) activity).validStartTime == false);
         });
 
         onView(withId(R.id.editTextStartTime)).perform(forceDoubleClick());
@@ -204,7 +255,7 @@ public class UploadActivityActivityTest {
         });
 
 
-        /*onView(withId(R.id.editTextDate)).perform(forceDoubleClick());
+        onView(withId(R.id.editTextDate)).perform(forceDoubleClick());
 
         onView(withClassName(equalTo(DatePicker.class.getName()))).perform(
                 PickerActions.setDate(
@@ -221,65 +272,13 @@ public class UploadActivityActivityTest {
 
         scenario.onActivity(activity -> {
             assert (((UploadActivityActivity) activity).validDate == true);
-        }); */
+        });
 
-        //mAuth.signOut();
         scenario.close();
 
-    }
+    }*/
 
-    @After
-    public void deleteAndSignOut() {
-        CountDownLatch latch = new CountDownLatch(1);
-
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-
-        backendActivityManager.search(ActivitySerializer.ORGANIZER_KEY, mAuth.getCurrentUser().getUid())
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (DocumentSnapshot snap: task.getResult().getDocuments()) {
-                                backendActivityManager.delete(snap.getReference().getPath());
-                            }
-                        }
-                    }
-                });
-
-        /*BackendActivityManager bam = new BackendActivityManager(db, BackendActivityManager.ACTIVITIES_COLLECTION);
-
-        bam.getActivitiesCollectionReference()
-                .whereEqualTo("organizerId", mAuth.getCurrentUser().getUid())
-                .get()
-                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                    @Override
-                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                        for (DocumentSnapshot docSnap: queryDocumentSnapshots.getDocuments()) {
-                            docSnap.getReference().delete()
-                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
-                                            // Silently fail if document couldn't be deleted ->
-                                            // prevent simultaneous deletions causing problems
-                                            latch.countDown();
-                                        }
-                                    });
-                        }
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                assert(false);
-            }
-        });*/
-
-
-
-        mAuth.signOut();
-    }
-
-    public boolean sleep(int millis) {
+    /*public boolean sleep(int millis) {
         try {
             Thread.sleep(millis);
             return true;
@@ -287,6 +286,6 @@ public class UploadActivityActivityTest {
             e.printStackTrace();
             return false;
         }
-    }
+    }*/
 
 }
