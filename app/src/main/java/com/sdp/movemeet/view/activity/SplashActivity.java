1d1e42d2e92f60d2b2abe24742aa50d6fde482ff
RoxaneBurri <roxane.burri@etu.unige.ch>
2021-06-03 21:11:34 +0200

    add animations

---
+++ app/src/main/java/com/sdp/movemeet/view/activity/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.sdp.movemeet.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sdp.movemeet.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
