f613d072c00cc312f24d38874ebf17bcce1a54b7
RoxaneBurri <roxane.burri@etu.unige.ch>
2021-06-04 17:43:06 +0200

    add animation

--- app/src/main/java/com/sdp/movemeet/view/activity/SplashActivity.java
+++ app/src/main/java/com/sdp/movemeet/view/activity/SplashActivity.java
@@ -1,16 +1,35 @@
 package com.sdp.movemeet.view.activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.airbnb.lottie.LottieAnimationView;
 import com.sdp.movemeet.R;
+import com.sdp.movemeet.view.home.HomeScreenActivity;
+import com.sdp.movemeet.view.home.LoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private LottieAnimationView foot;
+    private boolean isChecked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        /*foot = findViewById(R.id.foot);
+        foot.setSpeed(1);
+        foot.playAnimation();*/
+
+        getSupportActionBar().hide();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+
+        //startActivity(new Intent(this, HomeScreenActivity.class));
     }
 }
