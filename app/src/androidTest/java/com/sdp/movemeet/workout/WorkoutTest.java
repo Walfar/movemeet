9efc538f35a71974be7dd29789cc390f08340860
Mark <kotleta2007@protonmail.ch>
2021-06-04 22:37:58 +0200

    Fixed bugs, added tests

--- app/src/androidTest/java/com/sdp/movemeet/workout/WorkoutTest.java
+++ app/src/androidTest/java/com/sdp/movemeet/workout/WorkoutTest.java
@@ -1,14 +1,71 @@
 package com.sdp.movemeet.workout;
 
-import com.sdp.movemeet.utility.UserDatabase;
+import android.util.Log;
+
+import androidx.test.core.app.ActivityScenario;
+
+import com.sdp.movemeet.R;
+import com.sdp.movemeet.ui.workout.Model;
+import com.sdp.movemeet.ui.workout.TextWorkoutActivity;
+import com.sdp.movemeet.ui.workout.WorkoutActivity;
+import com.sdp.movemeet.view.profile.ProfileActivity;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import java.io.IOException;
 
 public class WorkoutTest {
     @Test
-    public void WorkoutIsCorrect() {
+    public void WorkoutModelIsCorrect() {
+        Model model = new Model("title", "description", 0);
 
+        assertEquals("title", model.getTitle());
+        assertEquals("description", model.getDesc());
+        assertEquals(0, model.getIcon());
+    }
+
+    @Test
+    public void WorkoutActivityIsCorrect() {
+        try (ActivityScenario<WorkoutActivity> scenario = ActivityScenario.launch(WorkoutActivity.class)) {
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                assert (false);
+            }
+
+            onView(withId(R.id.tabs)).perform(click());
+            onView(withId(R.id.listView)).perform(click());
+
+        }
+        catch (Exception e) {
+            Log.d("TAG", "Error message: " + e);
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void TextWorkoutActivityIsCorrect() {
+        try (ActivityScenario<TextWorkoutActivity> scenario = ActivityScenario.launch(TextWorkoutActivity.class)) {
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                assert (false);
+            }
+
+            //onView(withId(R.id.textView)).check(matches(withText(TEST_NAME)));
+
+        }
+        catch (Exception e) {
+            Log.d("TAG", "Error message: " + e);
+            e.printStackTrace();
+        }
     }
 }
