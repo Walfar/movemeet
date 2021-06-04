b86af444a0c1a7762c52f8388e044ac011024886
RoxaneBurri <roxane.burri@etu.unige.ch>
2021-06-04 18:30:31 +0200

    finish splash screen

--- app/src/main/java/com/sdp/movemeet/view/activity/SplashActivity.java
+++ app/src/main/java/com/sdp/movemeet/view/activity/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.sdp.movemeet.view.activity;
 
 import android.content.Intent;
+import android.nfc.cardemulation.HostNfcFService;
 import android.os.Bundle;
 import android.view.WindowManager;
 
@@ -13,7 +14,6 @@ import com.sdp.movemeet.view.home.LoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private LottieAnimationView foot;
     private boolean isChecked = false;
 
     @Override
@@ -21,15 +21,22 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        /*foot = findViewById(R.id.foot);
-        foot.setSpeed(1);
-        foot.playAnimation();*/
-
-        getSupportActionBar().hide();
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        Thread td = new Thread(){
+            public void run(){
+                try{
+                    sleep(4000);
+                }catch(Exception ex){
+                    ex.printStackTrace();
+                }
+                finally {
+                    Intent intent = new Intent(SplashActivity.this, HomeScreenActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };td.start();
 
-
-        //startActivity(new Intent(this, HomeScreenActivity.class));
     }
 }
